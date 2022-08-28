437d0b20015a565d4d19135b8c6eb32555e4eee9
Viola <asdfg2009-29-69@mail.ru>
2022-08-28 16:42:57 +0300

    yjdjt

---
+++ Z41/Program.cs
@@ -0,0 +1,75 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+//-1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+Console.WriteLine("ВВедите размер массива");
+int z = int.Parse(Console.ReadLine());            //размер массива
+
+int f = 0;
+int[] array = new int[z];
+
+int[] Index(int[] array, int z, int f)     //метод ввода чисел
+{
+    f = 0;
+    do
+    {
+        if (f < z)
+        {
+            int p = z;
+            Console.WriteLine($"Введите число");
+            array[f] = int.Parse(Console.ReadLine());
+            f++;
+        }
+    } while (f < z);
+    return array;
+}
+
+
+int[] CopyArray(int[] arr)                            //тут храню массив
+{
+    int size = arr.Length;
+    int[] newArr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        newArr[i] = arr[i];
+
+    }
+    return newArr;
+}
+
+void PrintArray(int[] arr)                                  //метод вывода массива со скобками (красивый массив)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == 0) Console.Write("{ ");
+        if (i < arr.Length - 1) Console.Write(arr[i] + " | ");
+        else Console.Write(arr[i] + " }");
+
+    }
+
+}
+
+int Number(int[] arr)                                        //метод пересчета пускай будет ядро
+{
+    int result = 0;                                          // счет результата начинается с 0
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) result = result + 1;                 //нахождение количества чисел, больше 0
+    }
+
+    return result;
+}
+
+Index(array, z, f);
+
+int[] newArray = CopyArray(array);
+Console.WriteLine();
+PrintArray(newArray);
+
+int number = Number(array);
+Console.WriteLine();
+Console.WriteLine($"Колличество элементов больше 0 = {number}");
---
+++ semenah/Program.cs
@@ -0,0 +1,323 @@
+﻿// Console.WriteLine("Введите числа через запятую: ");
+// string input = Console.ReadLine();
+
+// int[] numbers = ParseStringToArray(input);
+// PrintArray(numbers);
+
+// int[] ParseStringToArray(string input)
+// {
+//     int countNumbers = GetCountNumbersInString(input);
+//     int[] numbers = new int[countNumbers];
+//     int indexNumber = 0;
+//     string substring = string.Empty;
+//     for (int i = 0; i < input.Length; i++)
+//     {
+//         if (input[i] != ',')
+//         {
+//             substring += input[i];
+//         }
+//         else
+//         {
+//             numbers[indexNumber] = Convert.ToInt32(substring);
+//             indexNumber++;
+//             substring = string.Empty;
+//         }
+//     }
+//     if(input[input.Length - 1] != ',')
+//         numbers[indexNumber] = Convert.ToInt32(substring);
+//     return numbers;
+// }
+// int GetCountNumbersInString(string input) 
+// {
+//     int count;
+//     if(input[input.Length - 1] == ',')
+//         count = 0;
+//     else
+//         count = 1;
+    
+//     for(int i = 0; i < input.Length; i++)
+//     {
+//         if(input[i] == ',')
+//             count++;
+//     }
+//     return count;
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+// }
+
+
+//*********************************************************************************************************
+
+// int rows = ReadInt("Введите количество строк: ");
+// int columns = ReadInt("Введите количество столбцов: ");
+// int[,] numbers = new int[rows, columns];
+// FillArray2D(numbers);
+// PrintArray2D(numbers);
+
+// void FillArray2D(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(-5, 9);
+//         }
+//     }
+// }
+
+// void PrintArray2D(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+
+// ************************************************************************************************************************
+
+// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+// int ReadInt (string message)
+// {
+//     Console.Write (message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// void FillArray2D(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = i+j;
+//         }
+//     }
+// }
+
+// void PrintArray2D(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+// /*---------------MAIN--------*/
+
+// int rows = ReadInt("Введите количество строк: ");
+// int cols = ReadInt("Введите количество столбцов: ");
+// int[,] numbers = new int [rows,cols];
+// FillArray2D(numbers);
+// PrintArray2D(numbers);
+
+
+
+// *****************************************************************************************
+
+
+// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса нечётные, и замените эти элементы на их квадраты.
+// Например, изначально массив
+//  выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 
+// int rows = ReadInt("Введите количество строк: ");
+// int columns = ReadInt("Введите количество столбцов: ");
+// int [,] array = new int [rows,columns];
+
+// FillArray2D(array);
+// PrintArray2D(array);
+
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+//     for (int j = 0; j < array.GetLength(1);j++)
+//     {
+//         if (i % 2 ==1 && j % 2 ==1)
+//         {
+//             array [i,j] *= array[i,j]; 
+//         }
+
+//     }
+    
+// }
+// PrintArray2D(array);
+
+// void FillArray2D(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(0,10);
+//         }
+//     }
+// }
+
+// void PrintArray2D(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+// int ReadInt (string message)
+// {
+//     Console.Write (message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+
+
+// ********************************************************************************************************************************************
+
+// Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+
+// int rowsNumber = ReadInt("Введите количество строк: ");
+// int columnsNumber = ReadInt("Введите количество столбцов: ");
+// int[,] arrayNumbers = new int[rowsNumber, columnsNumber];
+// int sumElements = 0;
+
+// FillArray2D(arrayNumbers);
+
+// for (int i = 0; i < arrayNumbers.GetLength(0); i++)
+// {
+//     for (int j = 0; j < arrayNumbers.GetLength(1); j++)
+//     {
+//         if (i == j)
+//         {
+//             sumElements += arrayNumbers [i,j];
+//         }
+//     }
+// }
+
+// PrintArray2D(arrayNumbers);
+// Console.WriteLine (sumElements);
+
+// void FillArray2D(int[,] arrayNumbers)
+// {
+//     for (int i = 0; i < arrayNumbers.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arrayNumbers.GetLength(1); j++)
+//         {
+//             arrayNumbers[i, j] = new Random().Next(0, 10);
+//         }
+//     }
+// }
+
+// void PrintArray2D(int[,] aarrayNumbers)
+// {
+//     for (int i = 0; i < arrayNumbers.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arrayNumbers.GetLength(1); j++)
+//         {
+//             Console.Write(arrayNumbers[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+
+// int rows = ReadInt("Введите количество строк: ");
+// int columns = ReadInt("Введите количество столбцов: ");
+
+// int[,] numbers = new int[rows, columns];
+// FillArray2D(numbers);
+// PrintArray2D(numbers);
+// int sum = 0;
+
+// int rows = ReadInt("Введите количество строк: ");
+// int columns = ReadInt("Введите количество столбцов: ");
+
+// int[,] numbers = new int[rows, columns];
+// FillArray2D(numbers);
+// PrintArray2D(numbers);
+// int sum = 0;
+
+// for (int i = 0; i < numbers.GetLength(0); i++)
+// {
+//     for (int j = 0; j < numbers.GetLength(1); j++)
+//     {
+//         if(i == j)
+//         {
+//             sum += numbers[i, j];
+//             break;
+//         }
+//     }
+// }
+// Console.WriteLine();
+// Console.WriteLine(sum);
+
+// void FillArray2D(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(1, 9);
+//         }
+//     }
+// }
+// void PrintArray2D(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+
+
+
+
+
